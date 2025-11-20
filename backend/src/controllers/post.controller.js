7c52726394f98f6787b1a2d625c9e31dd2ced007
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-11-20 17:38:20 -0300

    main: feat (posts) - Implemented "updatePost" functionality and updated route definitions

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -1,20 +1,19 @@
 import { Post } from "../models/post.model.js";
 
-// Criação de uma postagem
 const createPost = async (req, res) => {
   try {
     const { name, description, age } = req.body;
 
-    if (!name || !description || !age) {
+    if (!name || !description || age === undefined) {
       return res.status(400).json({
-        message: "Todos os campos são requiridos.",
+        message: "Todos os campos são requeridos.",
       });
     }
 
     const post = await Post.create({ name, description, age });
 
     res.status(201).json({
-      message: "Postagem criado com sucesso!",
+      message: "Postagem criada com sucesso!",
       post,
     });
   } catch (error) {
@@ -25,8 +24,7 @@ const createPost = async (req, res) => {
   }
 };
 
-// Leitura de todas as postagens
-const getPosts = async (req, res) => {
+const getPosts = async (_req, res) => {
   try {
     const posts = await Post.find();
     res.status(200).json(posts);
@@ -38,4 +36,38 @@ const getPosts = async (req, res) => {
   }
 };
 
-export { createPost, getPosts };
+const updatePost = async (req, res) => {
+  try {
+    // Validação básica para verificar se o corpo (body) está vazio
+
+    // { name: x, description: y, age: z } -> [name, description, age]
+    // {} = truthy
+
+    if (Object.keys(req.body).length === 0) {
+      return res.status(400).json({
+        message: "Nenhum dado fornecido para atualização.",
+      });
+    }
+
+    const post = await Post.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+
+    if (!post)
+      return res.status(404).json({
+        message: "Postagem não encontrada.",
+      });
+
+    res.status(200).json({
+      message: "Postagem atualizada com sucesso!",
+      post,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Erro interno do servidor.",
+      error,
+    });
+  }
+};
+
+export { createPost, getPosts, updatePost };
